2912a2291a834c3319000e68f19a8ccd4e70aaa2
fakihariefnoto <fakihariefnoto@gmail.com>
2018-04-01 12:56:23 +0700

    add db connection and dep

--- util/connection/database/database.go
+++ util/connection/database/database.go
@@ -1,1 +1,12 @@
 package database
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var UserDB *sql.DB
+
+func Connect() (err error) {
+	UserDB, _ := sql.Open("sqlite3", "./nraboy.db")
+}
